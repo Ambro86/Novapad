098c7851012f517fb3b94f38976725ea7958d22a
Ambro86 <58132298+Ambro86@users.noreply.github.com>
2026-01-01 02:45:14 +0100

    Fix WiX template features and update CI workflow for MSI generation

---
+++ scripts/move_msi.ps1
@@ -0,0 +1,1 @@
+Copy-Item -Path "C:\rustnotepad\rustnotepad\target\release\.cargo-packager\wix\x64\output.msi" -Destination "C:\rustnotepad\rustnotepad\target\release\novapad_0.5.1_x64_en-US.msi" -Force
